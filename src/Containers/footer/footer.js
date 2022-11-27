ffd03f9944c490ed5f8f5d066071ad4e50ff72c2
hongdangcseiu <hongdang2k1@gmail.com>
2022-11-27 17:14:23 +0700

    bottom footer

--- src/Containers/footer/footer.js
+++ src/Containers/footer/footer.js
@@ -4,11 +4,12 @@ import './footer.css'
 function footer() {
 
     return (
-        <>
-            <footer className="fadeInUp col-md-12">  
-            </footer>
+        
+            <footer className="fadeInUp col-md-12" style={{position: 'absolute'}}>  
             <h5 className="text-center copyright"> © Copyrights by SE Team</h5>
-        </>
+            </footer>
+            
+        
     )
 }
 export default footer
